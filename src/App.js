33894d818a0a97ea0a102ca12c3c55fb19fcc28a
Iveta Círová <cirova.iveta@ptw.cz>
2023-11-22 09:41:36 +0100

    id + card.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Card from "./components/Card"
-import CardOpen from "./components/CardOpen"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import data from "./data"
@@ -14,17 +13,16 @@ const App = () => {
 
             <Header></Header>
 
-            <div className="content">
-
-                <CardOpen title={data[0].title} tags={data[0].tags} perex={data[0].perex} description={data[0].description} photo={data[0].photo} alt={data[0].alt}></CardOpen>
-               
-
-                <div className="cards">
-                    <Card title={data[0].title} tags={data[0].tags} perex={data[0].perex}></Card>
-                    <Card title={data[1].title} tags={data[1].tags} perex={data[1].perex}></Card>
-                    <Card title={data[2].title} tags={data[2].tags} perex={data[2].perex}></Card>
-                </div>
-
+            <div className="cards">
+                {
+                    data.map ( (oneCard) => {
+                        const {id, title, tags, perex} = oneCard
+                            
+                        return(    
+                            <Card key={id} id={id} title={title} tags={tags} perex={perex}></Card>     
+                        )
+                    })
+                }
             </div>
 
             <Footer></Footer>
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,14 @@ import Button from "./Button";
 
 
 const Card = (props) => {
-  return (
+
+  const {id, title, tags, perex} = props
+
+   return (
     <div className="card">
-      <h1 className="title">{ props.title }</h1>
-      <p className="tags">{props.tags}</p>
-      <p className="perex"> {props.perex}</p>
+      <h1 className="title">{title}</h1>
+      <p className="tags">{tags}</p>
+      <p className="perex">{perex}</p>
       <Button></Button>
     </div>
   );
